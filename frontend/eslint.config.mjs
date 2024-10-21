ea1016c5488ff525ded798b9abd2488010bad764
Daria <daryaab321@gmail.com>
2024-10-21 19:49:35 +0300

    fix eslint problem 2

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,11 +1,23 @@
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      'react/prop-types': 'off',
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
   pluginReact.configs.flat.recommended,
 ];
